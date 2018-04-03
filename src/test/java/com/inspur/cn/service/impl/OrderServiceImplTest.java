2f45d2cd4c7afe4eb0c54f45ae8fcb504180da61
manong110 <m15617927390_1@163.com>
2018-04-03 09:54:55 +0800

    订单服务测试类

--- src/test/java/com/inspur/cn/service/impl/OrderServiceImplTest.java
+++ src/test/java/com/inspur/cn/service/impl/OrderServiceImplTest.java
@@ -4,10 +4,13 @@ import com.inspur.cn.common.dto.OrderDto;
 import com.inspur.cn.common.dto.ResultDto;
 import com.inspur.cn.repo.OrderDetail;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
@@ -24,11 +27,18 @@ public class OrderServiceImplTest {
     private OrderServiceImpl orderService;
 
     private final String OPENID = "1234";
+
+    private final String ORDER_ID = "1522292169580988821";
+
+    private final String PRODUCT_ID = "1";
     @Test
     public void createOrder() throws Exception {
-        OrderDto orderDto = OrderDto.builder().address("成都").name("小米mix2S").phone("123456789")
+        OrderDto orderDto = OrderDto.builder().address("达州").name("小燕").phone("12345678923")
                 .openid(OPENID).build();
-        OrderDetail build = OrderDetail.builder().orderId("1").total(3).build();
+        OrderDetail build = OrderDetail.builder()
+                .orderId(ORDER_ID).productId(PRODUCT_ID)
+                .total(3).build();
+
         List<OrderDetail> orderDetails = new ArrayList<>();
         orderDetails.add(build);
 
@@ -39,18 +49,28 @@ public class OrderServiceImplTest {
 
     @Test
     public void findOrder() throws Exception {
+        OrderDto result = orderService.findOrder(ORDER_ID);
+        log.info("[查询单个订单] result-{}",result);
+        Assert.assertNotNull(result);
     }
 
     @Test
     public void findByOpenid() throws Exception {
+        PageRequest request = PageRequest.of(0,2);
+        Page<OrderDto> orderDtoPage = orderService.findByOpenid(OPENID, request);
+        Assert.assertNotNull(orderDtoPage.getTotalElements());
     }
 
     @Test
     public void cancel() throws Exception {
+        OrderDto order = orderService.findOrder(ORDER_ID);
+        OrderDto cancel = orderService.cancel(order);
+        Assert.assertEquals(new Integer(2),cancel.getOrderStatus());
     }
 
     @Test
     public void finish() throws Exception {
+
     }
 
     @Test
