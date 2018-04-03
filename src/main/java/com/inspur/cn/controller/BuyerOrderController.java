843393e275cf006979c059d7328ed2b6eec13723
manong110 <m15617927390_1@163.com>
2018-04-03 09:53:43 +0800

    BuyerOrderController 控制器

---
+++ src/main/java/com/inspur/cn/controller/BuyerOrderController.java
@@ -0,0 +1,21 @@
+package com.inspur.cn.controller;
+
+import com.inspur.cn.service.OrderService;
+import com.inspur.cn.service.ProductInfoService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/buyer/order")
+@Slf4j
+public class BuyerOrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private ProductInfoService productInfoService;
+
+}
